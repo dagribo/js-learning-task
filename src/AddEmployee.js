30149f3e7b5c1a31b295ec314554382879100e99
Bochkaryova DG <dagribo@yandex.ru>
2019-07-27 22:46:06 +0300

    remove unusable imports

--- src/AddEmployee.js
+++ src/AddEmployee.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import Reactable from 'reactable';
-import Req from './Requests';
 import axios from "axios";
 import { withRouter } from "react-router";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
